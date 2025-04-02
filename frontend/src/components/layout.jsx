d426f4b6ff1a1b3982785fa452e30c9fd58380ac
jluna2209 <83725494+jluna2209@users.noreply.github.com>
2025-04-02 15:45:25 -0400

    layout

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom'; // Import Outlet
-import Navbar from './Navbar'; 
+import Navbar from './Navbar.jsx'; 
 
 function Layout() {
   return (
