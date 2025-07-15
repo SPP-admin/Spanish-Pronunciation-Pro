020666855ffdd226e950129e9c44679fe2488345
oleedee <oneilleedee@gmail.com>
2025-07-15 18:59:28 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -52,8 +52,11 @@ app = FastAPI(
     docs_url= "/"
 )
 
+
+'''
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+'''
 
 origins = [
     "http://localhost:5173",
