7619577698f29cab439ca53cebe4d0d8b295ea06
SPP-admin <spanishpronunciationpro@gmail.com>
2025-08-15 17:50:02 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://spanish-pronunciation-pro.vercel.app"
+    "https://spanish-pronunciation-pro.vercel.app",
+	"https://spanish-pronunciation-pro-nine.vercel.app"
 ]
 
 app.add_middleware(
